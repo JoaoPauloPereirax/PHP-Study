b35467eb1e450da88c3f1f522d2ef9bfb324a4a9
João Paulo Pereira <joaopaulop.dev@gmail.com>
2024-04-23 20:39:38 -0300

    Updating arrays in the new study

--- 1.IntroduçãoPHP/Array/index.php
+++ 1.IntroduçãoPHP/Array/index.php
@@ -1,15 +1,24 @@
 <?php
 //Array
-$jogadores1=['Jogador 1','Jogador 2','Jogador 3','Jogador 4'];
+$jogadores1=[
+       'Jogador 1',
+       'Jogador 2',
+       'Jogador 3',
+       'Jogador 4'
+];
+
+
 echo "Player: ".$jogadores1[0]."<br>";
 echo "Player: ".$jogadores1[1]."<br>";
 echo "Player: ".$jogadores1[2]."<br>";
 echo "Player: ".$jogadores1[3]."<br>";
 
-$jogadores2=["Jogador",...$jogadores1];
+//A funcionalidade a seguir está presente somente depois da versão 7.4 no php
+$jogadores2=["Jogador Avulso",...$jogadores1];
+
 echo "Player: ".$jogadores2[0]."<br>";
 echo "Player: ".$jogadores2[1]."<br>";
 echo "Player: ".$jogadores2[2]."<br>";
 echo "Player: ".$jogadores2[3]."<br>";
 echo "Player: ".$jogadores2[4]."<br>";
-print_r($jogadores2);
+//print_r($jogadores2);
