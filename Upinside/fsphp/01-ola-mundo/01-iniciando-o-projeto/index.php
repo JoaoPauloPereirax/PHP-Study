62d88f3ffed48f07a007acb26d7896d9cc906e02
João Paulo Pereira <joaopaulop.dev@gmail.com>
2024-06-11 16:02:20 -0300

    Aula 1

---
+++ Upinside/fsphp/01-ola-mundo/01-iniciando-o-projeto/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>FSPHP: Iniciando Um Projeto</title>
+</head>
+<body>
+       <h1>Olá Mundo</h1>
+       <?php
+       $start = "Olá Mundo";
+       /* 
+       Bloco PHP
+       */
+
+       echo "{$start}, Vamos começar!";
+      ?>
+</body>
+</html>
